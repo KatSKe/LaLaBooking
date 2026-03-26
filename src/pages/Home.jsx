fe4e3d7c9eefe9181bc41944f2dcff69d8c0c362
KatSKe <katarina.skenderovic13@gmail.com>
2026-03-26 20:31:12 +0100

    ispravak

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,62 +4,19 @@ import animationData from "../assets/AISpark_InteractiveAssistant.lottie";
 
 export default function Home() {
     return (
-        <div
-            style={{
-                minHeight: "85vh",
-                position: "relative",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                textAlign: "center",
-                color: "white",
-                backgroundImage: "url('/images/bg.jpg')",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                padding: "20px"
-            }}
-        >
-            {/* overlay */}
-            <div
-                style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    backgroundColor: "rgba(255,255,255,0.05)",
-                    backdropFilter: "blur(4px)"
-                }}
-            />
+        <div className="hero">
+            <div className="overlay" />
 
-            {/* CONTENT */}
-            <div
-                style={{
-                    position: "relative",
-                    zIndex: 1,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: "10px"
-                }}
-            >
-                <h1
-                    style={{
-                        fontSize: "3.5rem",
-                        fontWeight: "800",
-                        letterSpacing: "2px",
-                        color: "#ffffff",
-                        textShadow: "2px 2px 15px rgba(0,0,0,0.6)",
-                        margin: 0
-                    }}
-                >
+            <div className="hero-content">
+
+                <h1 className="hero-title">
                     Welcome to {IME_APLIKACIJE}
                 </h1>
 
-                {/* LOTTIE ISPOD NASLOVA */}
-                <div style={{ width: "180px" }}>
+                <div className="lottie-wrapper">
                     <Lottie animationData={animationData} />
                 </div>
+
             </div>
         </div>
     );
