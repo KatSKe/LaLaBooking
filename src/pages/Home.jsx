9b9bcf98c7499af356d3ea22dd9fdad23fcd4512
KatSKe <katarina.skenderovic13@gmail.com>
2026-03-26 19:53:36 +0100

    dodan bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,44 @@ import { IME_APLIKACIJE } from "../constants";
 export default function Home(){
     return(
     <>
-    <h1>Welcome to {IME_APLIKACIJE}</h1>
-    <div>
-        add background
-    </div>
+    
+    <div style={{
+            minHeight: "85vh",
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            color: "white",
+            backgroundImage: "url('/images/bg.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center"
+        }}>
+
+                <div style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "rgba(255,255,255,0.05)",
+                backdropFilter: "blur(4px)"
+            }} />
+
+                <h1 style={{
+                position: "relative",
+                zIndex: 1,
+                fontSize: "3.5rem",
+                fontWeight: "800",
+                letterSpacing: "2px",
+                color: "#ffffff",
+                textShadow: "2px 2px 15px rgba(0,0,0,0.6)"
+            }}>
+                Welcome to {IME_APLIKACIJE}
+            </h1>
+
+        </div>
+    
     </>
     )
 }
