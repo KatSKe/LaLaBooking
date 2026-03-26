dea4578f28b0929a0bb4f8e4ebba729af2672abb
KatSKe <katarina.skenderovic13@gmail.com>
2026-03-26 20:24:26 +0100

    AISpark_InteractiveAssistant.lottie-responzive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,56 +2,65 @@ import { IME_APLIKACIJE } from "../constants";
 import Lottie from "lottie-react";
 import animationData from "../assets/AISpark_InteractiveAssistant.lottie";
 
-export default function Home(){
-    return(
-    <>
-    
-    <div style={{
-            minHeight: "85vh",
-            position: "relative",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            color: "white",
-            backgroundImage: "url('/images/bg.jpg')",
-            backgroundSize: "cover",
-            backgroundPosition: "center"
-        }}>
-
-            <div style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundColor: "rgba(255,255,255,0.05)",
-            backdropFilter: "blur(4px)"
-        }} />
-
-        <h1 style={{
-            position: "relative",
-            zIndex: 1,
-            fontSize: "3.5rem",
-            fontWeight: "800",
-            letterSpacing: "2px",
-            color: "#ffffff",
-            textShadow: "2px 2px 15px rgba(0,0,0,0.6)"
-        }}>
-            Welcome to {IME_APLIKACIJE}
-
-            <Lottie 
-                animationData={animationData}
+export default function Home() {
+    return (
+        <div
+            style={{
+                minHeight: "85vh",
+                position: "relative",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                color: "white",
+                backgroundImage: "url('/images/bg.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                padding: "20px"
+            }}
+        >
+            {/* overlay */}
+            <div
                 style={{
-                    height: 90,
-                    marginTop: "-10px",
-                    marginLeft: "708px"
-                }} 
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "rgba(255,255,255,0.05)",
+                    backdropFilter: "blur(4px)"
+                }}
             />
-        </h1>
 
-    </div>
-    
-    </>
-    )
+            {/* CONTENT */}
+            <div
+                style={{
+                    position: "relative",
+                    zIndex: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "10px"
+                }}
+            >
+                <h1
+                    style={{
+                        fontSize: "3.5rem",
+                        fontWeight: "800",
+                        letterSpacing: "2px",
+                        color: "#ffffff",
+                        textShadow: "2px 2px 15px rgba(0,0,0,0.6)",
+                        margin: 0
+                    }}
+                >
+                    Welcome to {IME_APLIKACIJE}
+                </h1>
+
+                {/* LOTTIE ISPOD NASLOVA */}
+                <div style={{ width: "180px" }}>
+                    <Lottie animationData={animationData} />
+                </div>
+            </div>
+        </div>
+    );
 }
